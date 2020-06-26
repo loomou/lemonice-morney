0aee034236b8558861c24debd7ce3a642ddbb095
loomou <273226981@qq.com>
2020-06-26 22:54:50 +0800

    创建<Layout>

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,7 @@
 <template>
-  <div>
-    Label.vue
-    <Nav/>
-  </div>
+    <Layout>
+      <p>Label.vue</p>
+    </Layout>
 </template>
 
 <script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      Money.vue
-    </div>
-    <Nav/>
-  </div>
+    <Layout>
+      <p>Money.vue</p>
+    </Layout>
 </template>
 
 <script>
@@ -19,15 +16,5 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper {
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-  .content {
-    border: 1px solid blue;
-    overflow: auto;
-    flex-grow: 1;
-  }
+
 </style>
