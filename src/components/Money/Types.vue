de160994e512486eb67c4d5f739b545b3e8586b6
loomou <273226981@qq.com>
2020-06-29 00:25:50 +0800

    完成Typescript组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -15,16 +15,33 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
+  // 1.自动提示更智能
+  // 2.不能随便写.toString()
+  // 3.编译报错，无法变成JS
   @Component
   export default class Types extends Vue {
     type = '-'; // '-'表示指出，'+'表示收入
+
+    // @Prop(Number) xxx: number | undefined;
+    // Prop告诉Vue，xxx不是data是prop
+    // Number告诉Vue xxx运行时是个Number
+    // xxx是属性名
+    // number | undefined告诉TS xxx的编译时类型
+
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
         throw new Error('type is unknown ');
       }
       this.type = type;
+    }
+    mounted() {
+      if (this.xxx === undefined) {
+        console.log('没有xxx');
+      } else {
+        console.log(this.xxx);
+      }
     }
   }
 
