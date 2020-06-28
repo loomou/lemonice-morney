b51ecd37cd7e33fc9e9fc230bf83e4ec69fe80f4
loomou <273226981@qq.com>
2020-06-28 22:07:41 +0800

    Money.vue模块化

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Types"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      /*line-height: 64px;*/
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected {
+        /*border-bottom: 4px solid; // 会将字顶上去，占位置*/
+        &::after {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 4px;
+          background: #333;
+        }
+      }
+    }
+  }
+</style>
