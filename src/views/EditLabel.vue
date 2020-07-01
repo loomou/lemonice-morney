852c9b1fe13fb7e96ba88ba2a0e7cadc7810fb71
loomou <273226981@qq.com>
2020-07-02 00:23:47 +0800

    解决id生成问题

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -54,11 +54,15 @@
 
     remove() {
       if (this.tag) {
-        tagListModel.remove(this.tag.id);
+        if (tagListModel.remove(this.tag.id)) {
+          this.$router.back();
+        } else {
+          window.alert('删除失败');
+        }
       }
     }
 
-    goBack(){
+    goBack() {
       this.$router.back();
     }
   }
