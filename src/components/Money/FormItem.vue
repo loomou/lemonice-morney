3f347c772f79020a15f0112d70a8369f6fa45535
loomou <273226981@qq.com>
2020-07-02 00:28:38 +0800

    修改bug

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -6,14 +6,14 @@
       <input type="text"
              :value="value"
              @input="onValueChange($event.target.value)"
-             :placeholder="this.placeholder">
+             :placeholder="placeholder">
     </label>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Watch, Prop} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class FormItem extends Vue {
@@ -22,7 +22,6 @@
     @Prop({required: true}) fieldName!: string;
     @Prop() placeholder!: string;
 
-    @Watch('value')
     onValueChange(value: string) {
       this.$emit('update:value', value);
     }
