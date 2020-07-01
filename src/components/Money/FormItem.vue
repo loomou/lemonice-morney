f62818c85a648e2f05c7b56af2a98dcc30a0e358
loomou <273226981@qq.com>
2020-07-01 23:17:52 +0800

    完成CSS，提取了几个通用组件

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label class="notes">
+    <label class="formItem">
       {{value}}
       <span class="name">{{this.fieldName}}</span>
       <!--      <input type="text" :value="value" @input="onInput" placeholder="在这里输入备注">-->
@@ -16,7 +16,7 @@
   import {Component, Watch, Prop} from 'vue-property-decorator';
 
   @Component
-  export default class Notes extends Vue {
+  export default class FormItem extends Vue {
     value = '';
 
     @Prop({required: true}) fieldName!: string;
@@ -35,9 +35,8 @@
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+  .formItem {
     font-size: 14px;
-    background: #f5f5f5;
     display: flex;
     padding-left: 16px;
     align-items: center;
@@ -47,7 +46,7 @@
     }
 
     input {
-      height: 64px;
+      height: 40px;
       flex-grow: 1;
       background: transparent;
       border: none;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,9 +2,11 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes field-name="备注"
-           placeholder="在这里输入备注"
-           @update:value="onUpdateNotes"/>
+    <div class="notes">
+      <FormItem field-name="备注"
+                placeholder="在这里输入备注"
+                @update:value="onUpdateNotes"/>
+    </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
@@ -14,33 +16,33 @@
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
+  import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/moudels/recordListMoudel'
+  import recordListModel from '@/moudels/recordListMoudel';
   import tagListModel from '@/moudels/tagListMoudel';
 
   const version = window.localStorage.getItem('version') || '0';
   const recordList: RecordItem[] = recordListModel.fetch();
   const tagList = tagListModel.fetch();
 
-    if (version === '0.0.1') {
-      // 数据库升级，数据迁移
-      recordList.forEach(record => {
-        record.createdAt = new Date(2020, 0, 1);
-      });
-      // 保存数据
-      window.localStorage.setItem('recordList', JSON.stringify(recordList));
-    }
+  if (version === '0.0.1') {
+    // 数据库升级，数据迁移
+    recordList.forEach(record => {
+      record.createdAt = new Date(2020, 0, 1);
+    });
+    // 保存数据
+    window.localStorage.setItem('recordList', JSON.stringify(recordList));
+  }
 
   window.localStorage.setItem('version', '0.0.2');
 
   @Component({
-    components: {Tags, Notes, Types, NumberPad}
+    components: {Tags, FormItem, Types, NumberPad}
   })
   export default class Money extends Vue {
     tags = tagList;
-    recordList: RecordItem[] =recordList;
+    recordList: RecordItem[] = recordList;
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
     onUpdateTags(value: string[]) {
@@ -68,6 +70,9 @@
   .layout-content {
     display: flex;
     flex-direction: column-reverse;
+  }
+  .notes {
+    padding: 12px 0;
   }
 </style>
 
