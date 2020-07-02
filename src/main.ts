9d688b6d89915346dd97dc53f772a9d1d20b8f5c
loomou <273226981@qq.com>
2020-07-03 00:29:07 +0800

    封装record的操作

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
 import tagListModel from '@/moudels/tagListMoudel';
+import recordListModel from '@/moudels/recordListMoudel';
 
 Vue.config.productionTip = false;
 
@@ -14,14 +15,29 @@ Vue.component('Nav', Nav);
 Vue.component('Layout', Layout);
 Vue.component('Icon', Icon);
 
+//record list
+window.recordList = recordListModel.fetch();
+window.createRecord = (record: RecordItem) => recordListModel.create(record);
+
+
+//tag list
 window.tagList = tagListModel.fetch();
+window.findTag = (id: string) => {
+  return window.tagList.filter(t => t, id === id)[0];
+};
 window.createTag = (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('标签名重复');
-    } else if (message === 'success') {
-      window.alert('添加成功');
-    }
+  const message = tagListModel.create(name);
+  if (message === 'duplicated') {
+    window.alert('标签名重复');
+  } else if (message === 'success') {
+    window.alert('添加成功');
+  }
+};
+window.removeTag = (id: string) => {
+  return tagListModel.remove(id);
+};
+window.updateTag = (id: string, name: string) => {
+  return tagListModel.update(id, name);
 };
 
 new Vue({
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,30 +18,25 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/moudels/recordListMoudel';
-  import tagListModel from '@/moudels/tagListMoudel';
+  import {Component} from 'vue-property-decorator';
 
-  const version = window.localStorage.getItem('version') || '0';
-  const recordList: RecordItem[] = recordListModel.fetch();
-
-  if (version === '0.0.1') {
-    // 数据库升级，数据迁移
-    recordList.forEach(record => {
-      record.createdAt = new Date(2020, 0, 1);
-    });
-    // 保存数据
-    window.localStorage.setItem('recordList', JSON.stringify(recordList));
-  }
-
-  window.localStorage.setItem('version', '0.0.2');
+  // const version = window.localStorage.getItem('version') || '0';
+  // if (version === '0.0.1') {
+  //   // 数据库升级，数据迁移
+  //   recordList.forEach(record => {
+  //     record.createdAt = new Date(2020, 0, 1);
+  //   });
+  //   // 保存数据
+  //   window.localStorage.setItem('recordList', JSON.stringify(recordList));
+  // }
+  // window.localStorage.setItem('version', '0.0.2');
 
   @Component({
     components: {Tags, FormItem, Types, NumberPad}
   })
   export default class Money extends Vue {
     tags = window.tagList;
-    recordList: RecordItem[] = recordList;
+    recordList = window.recordList;
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
     onUpdateTags(value: string[]) {
@@ -53,13 +48,9 @@
     }
 
     saveRecord() {
-      recordListModel.create(this.record);
+      window.createRecord(this.record);
     }
 
-    @Watch('recordList')
-    onRecordListChange() {
-      recordListModel.save();
-    }
   }
 </script>
 
