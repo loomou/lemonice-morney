cb6d71025db2cab2cfe3681aea441bd2fe6e5f17
loomou <273226981@qq.com>
2020-07-02 23:06:27 +0800

    将create封装到recordListModel

--- src/moudels/recordListMoudel.ts
+++ src/moudels/recordListMoudel.ts
@@ -1,14 +1,18 @@
+import clone from '@/lib/clone.ts';
+
 const localStorageKeyName = 'recordList';
 const recordListModel = {
   data: [] as RecordItem[],
-  clone(data: RecordItem[] | RecordItem) {
-    return JSON.parse(JSON.stringify(data));
+  create(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
   },
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     return this.data;
   },
-  save(data: RecordItem[]) {
+  save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
 };
