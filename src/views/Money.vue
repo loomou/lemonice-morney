0da4102f5e663edc7d2d01a1a295a4b40683389b
loomou <273226981@qq.com>
2020-07-04 23:11:32 +0800

    在Money.vue中使用Tabs.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,10 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+<!--    <Types :value.sync="record.type"/>-->
+    <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"
+    />
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
@@ -15,11 +18,11 @@
   // import Nav from '@/components/Nav.vue';
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
-
+  import Tabs from '@/components/Tabs.vue';
+  import recordTypeList from '@/constant/recordTypeList'
   // const version = window.localStorage.getItem('version') || '0';
   // if (version === '0.0.1') {
   //   // 数据库升级，数据迁移
@@ -32,7 +35,7 @@
   // window.localStorage.setItem('version', '0.0.2');
 
   @Component({
-    components: {Tags, FormItem, Types, NumberPad},
+    components: {Tabs, Tags, FormItem, NumberPad},
   })
   export default class Money extends Vue {
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
@@ -40,6 +43,8 @@
     get recordList() {
       return this.$store.state.recordList;
     }
+
+    recordTypeList = recordTypeList;
 
     created() {
       this.$store.commit('fetchRecords');
